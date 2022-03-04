fafe7ed6c525ed88ae0cbc50bd0494c0d63e1e18
Ruslan Sattarov <r8sattarov@mail.ru>
2022-03-04 20:24:08 +0500

    feat: add header
    This commit add header and also new directory - with js file.
    Add function for open burger menu.

---
+++ rsschool-cv-html/js/script.js
@@ -0,0 +1,8 @@
+// --------------- menu burger ----------- //
+const menuActive = document.querySelector('.menu-header__menu');
+const menuIcon = document.querySelector('.menu-header__icon');
+
+menuIcon.addEventListener ('click', function (e) {
+   menuActive.classList.toggle ('active');
+   menuIcon.classList.toggle ('icon-active');
+})
